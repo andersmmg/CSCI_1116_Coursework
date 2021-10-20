a73c00f1e741ed3ff2dc361152b78b2f9df108fd
404kevnotfound <andersmmg@gmail.com>
2021-10-20 13:42:44 -0600

    Chapter 24

---
+++ 24/Exercise24_05/GenericQueue.java
@@ -0,0 +1,19 @@
+public class GenericQueue<E> extends java.util.LinkedList<E> {
+
+	public void enqueue(E e) {
+		addLast(e);
+	}
+	
+	public E dequeue() {
+		return removeFirst();
+	}
+	
+	public int getSize() {
+		return size();
+	}
+
+	@Override
+	public String toString() {
+		return "Queue: " + super.toString();
+	}
+}
